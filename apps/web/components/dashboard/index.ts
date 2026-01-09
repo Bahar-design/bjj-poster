c47e345f74bd93bb1dcaa52e18fddfbad81004a4
Bahar Abdi <bahar.abdi04@gmail.com>
2026-01-09 00:40:30 -0600

    feat(dashboard): export UsageCard from index (ODE-72)

--- apps/web/components/dashboard/index.ts
+++ apps/web/components/dashboard/index.ts
@@ -1,3 +1,4 @@
 export { CreateNewCard } from './create-new-card';
 export { DashboardHeader } from './dashboard-header';
+export { UsageCard } from './usage-card';
 export { WelcomeSection } from './welcome-section';
